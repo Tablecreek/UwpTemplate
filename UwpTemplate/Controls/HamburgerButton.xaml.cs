33f1e50ff285dbb2a011dd8e4387acdeb7349342
Alain Tellenbach <Alain.Tellenbach@dolphin.ch>
2016-06-21 08:48:48 +0200

    Initial Commit

---
+++ UwpTemplate/Controls/HamburgerButton.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace UwpTemplate.Controls {
+
+    public sealed partial class HamburgerButton : UserControl {
+
+        private Modes _mode;
+
+        public event TappedEventHandler HamburgerButtonClicked;
+
+        public event TappedEventHandler BackButtonClicked;
+
+        public Modes Mode {
+            get {
+                return _mode;
+            }
+            set {
+                _mode = value;
+
+                switch (_mode) {
+                    case Modes.Hamburger:
+                        //TODO: Show Hamburger-Button
+                        break;
+                    case Modes.Back:
+                        //TODO: Show Back-Button
+                        break;
+                    case Modes.None:
+                        //TODO: Show None
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public HamburgerButton() {
+            InitializeComponent();
+        }
+
+        private void HamburgerButtonTapped(object sender, TappedRoutedEventArgs e) {
+            switch (_mode) {
+                case Modes.Hamburger:
+                    HamburgerButtonClicked?.Invoke(sender, e);
+                    break;
+                case Modes.Back:
+                    BackButtonClicked?.Invoke(sender, e);
+                    break;
+                case Modes.None:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public enum Modes {
+
+            Hamburger,
+            Back,
+            None
+
+        }
+
+    }
+
+}
